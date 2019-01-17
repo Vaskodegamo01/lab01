504a9aa6c2b0ed1b361b288274f226f6bc09aef1
vaskodegamo <vaskodegamo01@gmail.com>
2019-01-17 16:29:33 +0600

    остался только запрос с параметром дата и клиент

--- app/messages.js
+++ app/messages.js
@@ -1,19 +1,30 @@
 import express from "express";
+import nanoid from "nanoid";
 import fsDemo from "../fsDemo";
 
 const  router = express.Router();
 
 router.get("/:messages", (req, res) => {
-    if(req.params.messages === "messages"){ //делаем проверку правильный ли нам пришел запрос
-        //res.send(fsDemo.readFile());
-        fsDemo.readFile(()=>{res.send(fsDemo.data);
+    if(!req.query.datatime){
+        fsDemo.readFileAll(()=>{res.send(fsDemo.data);
         })
     }else{
-        res.send("неправильный запрос");
+        if(req.query.datatime.getData() === NaN){
+            res.status(400).send({error: "Time have bad format"})
+        }
+        else{
+            fsDemo.readFileAll(()=>{res.send(fsDemo.data);
+            }, req.query.datatime.toISOString())
+        }
     }
 });
 
 router.post("/messages", (req, res) => {
+    if(req.body.author == "" || req.body.message == ""){
+        res.status(400).send({error: "Author and message must be present in request"});
+        return;
+    }
+    req.body.id = nanoid(10);
     req.body.datetime = new Date();
     fsDemo.addItem(req.body);
     fsDemo.saveData(()=>res.send(fsDemo.data));
--- fsDemo.js
+++ fsDemo.js
@@ -3,25 +3,59 @@ const path = "./messages/";
 
 export default {
     data: [],
-    readFile(callback){
-            fs.readdir(path, (err, files) => {
-                if (err) {
-                    throw err;
-                }
-                let control=0;
-                files.reverse().forEach(file => {
-                    fs.readFile(path + file, "utf8", (err, res) => {
-                        if (err) {
-                            throw err;
-                        }
-                        this.data.push(JSON.parse(res)[0]);
-                        ++control;
-                        if (control === 5) {  //ждем пока 5 последних файлов будут прочитаны
-                            callback();
-                        }
+    readFileAll(callback,isodata){
+            if(!isodata){
+                fs.readdir(path, (err, files) => {
+                    if (err) {
+                        throw err;
+                    }
+                    let control=0;
+                    let file = [];
+                    let thirtyFiles = files.reverse();
+                    for(let i=0; i<30; i++) {
+                        file.push(thirtyFiles[i]);
+                    }
+                    file.sort((a,b) => (a > b) ? 1 : ((b> a) ? -1 : 0));
+                    file.sort().forEach(file => {
+                        fs.readFile(path + file, "utf8", (err, res) => {
+                            if (err) {
+                                throw err;
+                            }
+                            this.data.push(JSON.parse(res)[0]);
+                            ++control;
+                            if (control === 30) {  //читаем 30 файлов
+                                callback();
+                            }
+                        });
                     });
                 });
-            });
+            }else{
+                fs.readdir(path, (err, files) => {
+                    if (err) {
+                        throw err;
+                    }
+                    let control=0;
+                    let file = [];
+                    let dateFiles = files.reverse();
+                    for(let i=0; i<dateFiles.length; i++) {
+
+                        file.push(thirtyFiles[i]);
+                    }
+                    file.sort((a,b) => (a > b) ? 1 : ((b> a) ? -1 : 0));
+                    file.sort().forEach(file => {
+                        fs.readFile(path + file, "utf8", (err, res) => {
+                            if (err) {
+                                throw err;
+                            }
+                            this.data.push(JSON.parse(res)[0]);
+                            ++control;
+                            if (control === 30) {  //читаем 30 файлов
+                                callback();
+                            }
+                        });
+                    });
+                });
+            }
     },
     addItem(item){
         this.data.push(item);
